c4f9301fd6b084dfd7d142cf2a1947aa6b87fa27
Leonardo <leonardothelacerda@gmail.com>
2016-12-16 11:57:46 -0300

    creating inital components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-item.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
